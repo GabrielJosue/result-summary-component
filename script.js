7be4be7dfb3a21d5319ecd13754997c8a9982818
GabrielJosue <gj_alvarado@outlook.com>
2023-04-11 20:35:40 -0600

    se agrega atributo alt a las imagenes

--- script.js
+++ script.js
@@ -1,34 +1,36 @@
-'use Strict';
+"use Strict";
 
-const listEl = document.querySelector('.summary-list');
-async function renderData(){
-     await fetch('data.json').then((res) => {
-        return res.json();
-     }).then(data => {
-        console.log(data)
-        let summary = data;
-        summary.map(listItem => {
-            let liEl = document.createElement('li');
-            liEl.classList.add(`summary--${listItem.category}`);
-            let imgEl = document.createElement('img');
-            imgEl.setAttribute('src', `${listItem.icon}`)
-            let spanEl = document.createElement('span');
-            let spanEl2 = document.createElement('span');
-            let spanEl3 = document.createElement('span');
-            spanEl3.classList.add('summary-score');
-            spanEl3.textContent = `${listItem.score}`;
-            spanEl3.appendChild(spanEl2);
-            spanEl2.textContent = ` / 100`;
-            
-            spanEl.classList.add('summary-container');
-            spanEl.textContent = `${listItem.category}`
-            spanEl.appendChild(imgEl);
-            liEl.appendChild(spanEl3);
-            liEl.appendChild(spanEl);
-            listEl.appendChild(liEl);
-          
-        })
-     })
+const listEl = document.querySelector(".summary-list");
+async function renderData() {
+  await fetch("data.json")
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      console.log(data);
+      let summary = data;
+      summary.map((listItem) => {
+        let liEl = document.createElement("li");
+        liEl.classList.add(`summary--${listItem.category}`);
+        let imgEl = document.createElement("img");
+        imgEl.setAttribute("src", `${listItem.icon}`);
+        imgEl.setAttribute("alt", `icon-${listItem.category}`);
+        let spanEl = document.createElement("span");
+        let spanEl2 = document.createElement("span");
+        let spanEl3 = document.createElement("span");
+        spanEl3.classList.add("summary-score");
+        spanEl3.textContent = `${listItem.score}`;
+        spanEl3.appendChild(spanEl2);
+        spanEl2.textContent = ` / 100`;
+
+        spanEl.classList.add("summary-container");
+        spanEl.textContent = `${listItem.category}`;
+        spanEl.appendChild(imgEl);
+        liEl.appendChild(spanEl3);
+        liEl.appendChild(spanEl);
+        listEl.appendChild(liEl);
+      });
+    });
 }
 
 const data = renderData();
@@ -48,6 +50,6 @@ const data = renderData();
 //     //     console.log(list)
 //     // });
 // }
-console.log(data)
+console.log(data);
 // createListItems(data);
 //  console.log(listEl);
